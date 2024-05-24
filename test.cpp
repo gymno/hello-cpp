94d9124d39d5ed4411d92213dd85c26f1ccb7c21
gymno <gymnopedien@gmail.com>
2024-05-24 14:37:00 +0900

    Create main

---
+++ test.cpp
@@ -0,0 +1,13 @@
+// This is a test file for github-gymno
+
+#include <cstdio>
+
+void MyTest () {
+    int* aaa = new int;
+    *aaa = 120;
+}
+
+int main(int argc, char* argv[]) {
+    printf("hello cpp\n");
+    return 0;
+}
